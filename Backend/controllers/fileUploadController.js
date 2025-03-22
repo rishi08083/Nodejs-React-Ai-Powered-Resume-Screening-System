bdbdfe0a19576e9e580e6cf31ca1954527f4a207
vinay danidhariya <vinaydanidhariya4114@gmail.com>
2025-03-22 09:42:51 +0530

    Updated job creation and resume upload api

---
+++ Backend/controllers/fileUploadController.js
@@ -0,0 +1,77 @@
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const multer = require("multer");
+const path = require("path");
+const crypto = require("crypto");
+const db = require("../models/");
+
+require("dotenv").config();
+
+// AWS S3 Configuration
+const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+const generateFileName = (originalName) => {
+  const ext = path.extname(originalName);
+  return `${crypto.randomBytes(10).toString("hex")}${ext}`;
+};
+
+// Upload Multiple Resumes API
+exports.uploadResumes = async (req, res) => {
+  try {
+    if (!req.files || req.files.length === 0) {
+      return res.status(400).json({ error: "No files uploaded" });
+    }
+
+    // Validate AWS credentials and region
+    if (
+      !process.env.AWS_ACCESS_KEY_ID ||
+      !process.env.AWS_SECRET_ACCESS_KEY ||
+      !process.env.AWS_REGION
+    ) {
+      throw new Error(
+        "AWS credentials or region are not properly configured in the .env file"
+      );
+    }
+
+    const uploadedFiles = [];
+
+    for (const file of req.files) {
+      const fileName = generateFileName(file.originalname);
+      const params = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: fileName,
+        Body: file.buffer, // Ensure this is the file buffer
+        ContentType: file.mimetype
+      };
+
+      await s3.send(new PutObjectCommand(params));
+
+      const fileUrl = `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${fileName}`;
+      uploadedFiles.push({ fileName, fileUrl });
+
+
+    }
+    const unparsedResumes = uploadedFiles.map((file) => ({
+      user_id: req.user.id,
+      resume_url: file.fileUrl,
+      status: "uploaded",
+      is_deleted: false,
+    }));
+
+    await db.UnparsedResume.bulkCreate(unparsedResumes);
+
+    res
+      .status(200)
+      .json({ message: "Files uploaded successfully", files: uploadedFiles });
+  } catch (error) {
+    console.error("Error uploading files:", error);
+    res
+      .status(500)
+      .json({ error: "File upload failed", details: error.message });
+  }
+};
