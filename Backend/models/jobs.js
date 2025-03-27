a754699efc019c7c82d8aa3fa7bdef1a38e699b5
Nidhi Dhinoja <nidhidhinoja21@gmail.com>
2025-03-27 10:43:37 +0530

    Updated jobs table with new requirements

--- Backend/models/jobs.js
+++ Backend/models/jobs.js
@@ -4,20 +4,17 @@ const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Jobs extends Model {
     static associate(models) {
-      // One-to-Many: Jobs → Candidates
       Jobs.hasMany(models.Candidates, {
         foreignKey: "job_id",
         as: "candidates",
       });
 
-      // One-to-Many: Jobs → ScreeningResults
       Jobs.hasMany(models.ScreeningResults, {
         foreignKey: "job_id",
         as: "screening_results",
       });
 
-      // Many-to-One: Jobs → Users
-      Jobs.belongsTo(models.Users, { foreignKey: "user_id", as: "users" });
+      Jobs.belongsTo(models.Users, { foreignKey: "user_id", as: "user" });
     }
   }
 
@@ -27,6 +24,21 @@ module.exports = (sequelize, DataTypes) => {
       description: DataTypes.TEXT,
       location: DataTypes.STRING,
       experience_required: DataTypes.INTEGER,
+      job_type: DataTypes.STRING,
+      openings: DataTypes.INTEGER,
+      company_name: DataTypes.STRING,
+      skills_required: DataTypes.TEXT,
+      contact_info: DataTypes.STRING,
+      salary_range: DataTypes.STRING,
+      application_deadline: DataTypes.DATE,
+      is_rcd_uploaded: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      rcd_url: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
       user_id: DataTypes.INTEGER,
       is_deleted: DataTypes.BOOLEAN,
     },
