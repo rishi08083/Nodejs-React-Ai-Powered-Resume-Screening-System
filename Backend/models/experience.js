53cb9dd95a9f540da9dbe412629b7d58156fef49
Niket Pataditya <nik84soni@gmail.com>
2025-03-29 19:33:31 +0530

    Hotfix

--- Backend/models/experience.js
+++ Backend/models/experience.js
@@ -15,8 +15,8 @@ module.exports = (sequelize, DataTypes) => {
   Experience.init(
     {
       candidate_id: DataTypes.INTEGER,
-      company_names: DataTypes.ARRAY(DataTypes.STRING), // Changed from company_name
-      job_titles: DataTypes.ARRAY(DataTypes.STRING), // Changed from role
+      company_names: DataTypes.STRING, // Changed from company_name
+      job_titles: DataTypes.STRING, // Changed from role
       start_date: DataTypes.DATE,
       end_date: DataTypes.DATE,
       is_deleted: DataTypes.BOOLEAN,
