d82062d18f8e052d127c9faedca2d5ac74783de6
Sudipto <dassudipto200@gmail.com>
2025-03-30 23:59:33 +0530

    resolving cors error

--- Backend/middlewares/securityMiddleware.js
+++ Backend/middlewares/securityMiddleware.js
@@ -15,7 +15,7 @@ module.exports = (app) => {
         const allowedOrigins = [
           "http://localhost:3000",
           "http://localhost:3001",
-          "http://181.214.44.15:3001",
+          "http://181.214.44.15:3000",
           // Add your production domain when ready
         ];
 
@@ -46,13 +46,13 @@ module.exports = (app) => {
             "https://trusted.cdn.com",
             "http://localhost:3000",
             "http://localhost:3001",
-            "http://181.214.44.15:3001",
+            "http://181.214.44.15:3000",
           ],
           connectSrc: [
             "'self'",
             "http://localhost:3000",
             "http://localhost:3001",
-            "http://181.214.44.15:3001",
+            "http://181.214.44.15:3000",
           ],
           objectSrc: ["'none'"],
           imgSrc: ["'self'", "data:"],
