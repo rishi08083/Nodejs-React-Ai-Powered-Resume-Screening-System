8a40b62f5568f6bddb1f17a6bef5debe4a94a408
Rishi <shoririshi595@gmail.com>
2025-03-30 16:22:13 +0530

    Configured eslint

--- Frontend/eslint.config.mjs
+++ Frontend/eslint.config.mjs
@@ -1,22 +1,14 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals"),
-  {
-    rules: {
-      "react/no-unescaped-entities": "off", // Disable JSX unescaped entity error
-      "react-hooks/exhaustive-deps": "off", // Show as warning instead of error
-    },
-  },
-];
-
-export default eslintConfig;
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+]);
