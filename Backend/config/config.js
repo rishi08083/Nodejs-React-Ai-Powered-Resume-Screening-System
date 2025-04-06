60ccaa93ad211255b2c9f7e57f40072488938e96
vinay danidhariya <vinaydanidhariya4114@gmail.com>
2025-04-07 02:15:15 +0530

    #fix

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -16,13 +16,13 @@ const config = {
     database: process.env.PROD_DB_NAME,
     host: process.env.PROD_DB_HOSTNAME,
     port: process.env.PROD_DB_PORT,
-    dialect: process.env.PROD_DB_DIALECT || "postgres",
-     dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // Required for Render, AWS, etc.
-      },
-    },
+    // dialect: process.env.PROD_DB_DIALECT || "postgres",
+    //  dialectOptions: {
+    //   ssl: {
+    //     require: true,
+    //     rejectUnauthorized: false, // Required for Render, AWS, etc.
+    //   },
+    // },
    logging: false,
   },
 };
