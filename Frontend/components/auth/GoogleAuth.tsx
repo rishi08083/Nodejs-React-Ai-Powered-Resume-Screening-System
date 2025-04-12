ef20ed62b91e8092efb6b59d2fb685d79c0aa044
Niket Pataditya <nik84soni@gmail.com>
2025-04-12 14:25:11 +0530

    google button size Updated

--- Frontend/components/auth/GoogleAuth.tsx
+++ Frontend/components/auth/GoogleAuth.tsx
@@ -92,9 +92,10 @@ export default function GoogleSignIn({
     <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
       <div className="flex items-center justify-center w-full">
       <GoogleLogin
+        type="standard"
         theme="filled_black"
         size="large"
-        width={200}
+        width="335"
         onSuccess={handleGoogleSuccess}
         onError={() => {
           toast.error("Google login failed");
