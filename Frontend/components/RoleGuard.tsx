31165af0354e9f0517c9f224ca19b77aea940647
vinay danidhariya <vinaydanidhariya4114@gmail.com>
2025-04-20 18:08:10 +0530

    #fix 2

--- Frontend/components/RoleGuard.tsx
+++ Frontend/components/RoleGuard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useAuth } from "@/lib/auth";
+import { useAuth } from "../lib/auth";
 import { ReactNode } from "react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
